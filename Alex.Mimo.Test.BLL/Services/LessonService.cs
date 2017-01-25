5713b4448f79d7455aa9ba51048833ebc3488a98
zhdenas <zhdenas@gmail.com>
2017-01-26 00:12:06 +0300

    lessons service impol

--- Alex.Mimo.Test.BLL/Services/LessonService.cs
+++ Alex.Mimo.Test.BLL/Services/LessonService.cs
@@ -1,20 +1,30 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Alex.Mimo.Test.BLL.Contracts;
 using Alex.Mimo.Test.BLL.Models;
+using Alex.Mimo.Test.DataAccess.Repositories.Contracts;
 
 namespace Alex.Mimo.Test.BLL.Services
 {
     public class LessonService : ILessonService
     {
+        private ILessonRepository _lessonRepository;
+
+        public LessonService(ILessonRepository lessonRepository)
+        {
+            this._lessonRepository = lessonRepository;
+        }
+
         public void SetCompleted(CheckLessonModel checkLesson)
         {
             throw new NotImplementedException();
         }
 
-        public Task SetCompletedAsync(CheckLessonModel checkLesson)
+        public async Task SetCompletedAsync(CheckLessonModel checkLesson, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            await this._lessonRepository.CompleteLesson(checkLesson.UserId, checkLesson.Id, checkLesson.StartedDate,
+                checkLesson.FinishedDate, cancellationToken);
         }
     }
 }
