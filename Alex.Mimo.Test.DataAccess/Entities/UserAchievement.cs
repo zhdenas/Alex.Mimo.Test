4d478197993fcaff3969c95e6f6baca2b1dfc4a9
zhdenas <zhdenas@gmail.com>
2017-01-25 19:07:27 +0300

    Added missing models

---
+++ Alex.Mimo.Test.DataAccess/Entities/UserAchievement.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Alex.Mimo.Test.DataAccess.Entities
+{
+    public class UserAchievement
+    {
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public virtual User User { get; set; }
+
+        public string UserId { get; set; }
+
+        [ForeignKey(nameof(AchievementId))]
+        public virtual Achievement Achievement { get; set; }
+
+        public string AchievementId { get; set; }
+    }
+}
